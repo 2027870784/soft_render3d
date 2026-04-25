21fe178f2e66295eed5e9ca2679507a52cba3dab
DemoDog <123@DemoDog.com>
2026-04-25 15:03:18 +0800

    新增了Triangle类

---
+++ renderer/Triangle.cpp
@@ -0,0 +1,56 @@
+#include "Triangle.hpp"
+#include <iostream>
+
+Triangle::Triangle(){
+    vex[0] = Vector4(0,0,0,1);
+    vex[1] = Vector4(0,0,0,1);
+    vex[2] = Vector4(0,0,0,1);
+
+    normal = Vector3(0,0,0);
+    
+    color[0] = Vector3(0,0,0);
+    color[1] = Vector3(0,0,0);
+    color[2] = Vector3(0,0,0);
+}
+
+Triangle::Triangle(const std::array<Vector4,3>& vexs, const std::array<Vector3,3>& colors, const Vector3& n){
+    setVexs(vexs);
+    setColors(colors);
+    setNormal(n);
+}
+
+Triangle::~Triangle() = default;
+
+void Triangle::setVex(int index, const Vector4& vex){
+    this->vex[index] = vex;
+}
+
+void Triangle::setColor(int index, float r, float g, float b){
+    if((r<0.0) || (r>255.) ||
+       (g<0.0) || (g>255.) ||
+       (b<0.0) || (b>255.)) {
+        std::cout << "Error: color value out of range." << std::endl;
+        return;
+    }
+    color[index] = Vector3(r,g,b);
+}
+
+void Triangle::setColor(int index, const Vector3& col){
+    setColor(index, col.r(), col.g(), col.b());
+}
+
+void Triangle::setNormal(const Vector3& n){
+    normal = n;
+}
+
+void Triangle::setVexs(const std::array<Vector4,3>& vexs){
+    vex[0] = vexs[0];
+    vex[1] = vexs[1];
+    vex[2] = vexs[2];
+}
+
+void Triangle::setColors(const std::array<Vector3,3>& colors){
+    color[0] = colors[0];
+    color[1] = colors[1];
+    color[2] = colors[2];
+}
---
+++ renderer/Triangle.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "math3d.hpp"
+#include <array>
+
+class Triangle {
+public:
+    Vector4 vex[3];//三角形初始顶点坐标
+    Vector3 color[3];//三角形顶点颜色
+    Vector3 normal;//三角形法线向量（针对整个三角形面）
+    //使用顶点着色时，要将顶点相邻的所有三角形面的法线向量进行平均处理作为该顶点的法线向量
+    //此处法线定义不适用于片元着色，使用片元着色时，需要给每个顶点定义法线向量
+
+    Triangle();
+    Triangle(const std::array<Vector4,3>& vexs, const std::array<Vector3,3>& colors, const Vector3& n);
+    ~Triangle();
+
+    //返回三角形顶点坐标
+    Vector4 a() const {return vex[0];}
+    Vector4 b() const {return vex[1];}
+    Vector4 c() const {return vex[2];}
+
+    //返回三角形法线向量
+    Vector3 getNormal() const {return normal;}
+
+    void setVex(int index, const Vector4& vex);
+    void setColor(int index, const Vector3& col);
+    void setColor(int index, float r, float g, float b);
+    void setNormal(const Vector3& n);
+
+    void setVexs(const std::array<Vector4,3>& vexs);
+    void setColors(const std::array<Vector3,3>& colors);
+
+};
